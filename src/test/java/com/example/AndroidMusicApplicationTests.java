3488226bd5c7a93d1426f6beac246ad26639e433
luoyurong <2407944371@qq.com>
2024-04-04 12:10:13 +0800

    测试类

--- src/test/java/com/example/AndroidMusicApplicationTests.java
+++ src/test/java/com/example/AndroidMusicApplicationTests.java
@@ -1,14 +1,11 @@
 package com.example;
 
-import com.example.config.HttpRequestUtils;
 import com.example.entity.User;
 import com.example.service.UserService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
 
 @SpringBootTest
 @Slf4j
